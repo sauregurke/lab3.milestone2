4809f7eeee5a7f91e8c2543955702e0c20685f19
Christopher Verbsky <chrisverbsky@gmail.com>
2023-09-30 23:13:53 -0500

    Complete

--- app/src/main/java/com/cs407/lab3milestone2/MainActivity.java
+++ app/src/main/java/com/cs407/lab3milestone2/MainActivity.java
@@ -1,8 +1,11 @@
 package com.cs407.lab3milestone2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fragManager = getSupportFragmentManager();
+
+        Button firstFragmentButton = findViewById(R.id.firstFragmentButton);
+        firstFragmentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView, FirstFragment.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack("showing First")
+                        .commit();
+            }
+        });
+        Button secondFragmentButton = findViewById(R.id.secondFragmentButton);
+        secondFragmentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fragManager.beginTransaction()
+                        .replace(R.id.fragmentContainerView, SecondFragment.class, null)
+                        .setReorderingAllowed(true)
+                        .addToBackStack("showing Second")
+                        .commit();
+            }
+        });
     }
 }
